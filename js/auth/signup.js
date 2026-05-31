cc49ca5206aeab576fa85584bc339777cfa0bb98
David Lgc <legouellecd@gmail.com>
2026-05-31 19:15:38 +0200

    feat(signup):create function regex validate Password

--- js/auth/signup.js
+++ js/auth/signup.js
@@ -14,6 +14,7 @@ function validateForm() {
   const nomOk = validateRequired(inputNom);
   const prenomOk =validateRequired(inputPreNom);
   const mailOk = validateRequired(inputMail);
+  const passwordOk = validatePassword(inputPassword);
 
   if(nomOk && prenomOk && mailOk){
     btnValidation.disabled = false
@@ -50,17 +51,17 @@ function validateRequired(input) {
 }
 
 
-// function validatePassword(input){
-//     const passwordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[\W_])[A-Za-z\d\W_]{8,}$/;
-//     const passwordUser = input.value;
-//     if(passwordUser.match(passwordRegex)){
-//         input.classList.add("is-valid");
-//         input.classList.remove("is-invalid"); 
-//         return true;
-//     }
-//     else{
-//         input.classList.remove("is-valid");
-//         input.classList.add("is-invalid");
-//         return false;
-//     }
-// }
+function validatePassword(input){
+    const passwordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[\W_])[A-Za-z\d\W_]{8,}$/;
+    const passwordUser = input.value;
+    if(passwordUser.match(passwordRegex)){
+        input.classList.add("is-valid");
+        input.classList.remove("is-invalid"); 
+        return true;
+    }
+    else{
+        input.classList.remove("is-valid");
+        input.classList.add("is-invalid");
+        return false;
+    }
+}
