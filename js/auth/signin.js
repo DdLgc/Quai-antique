5c900c6b4c74b67f2c1402dcdd22e1d3edca3d44
David Lgc <legouellecd@gmail.com>
2026-06-01 12:53:15 +0200

    feat(script)create token & connexion

---
+++ js/auth/signin.js
@@ -0,0 +1,19 @@
+const mailInput = document.getElementById("EmailInput");
+const passwordInput = document.getElementById("PasswordInput");
+const btnSignin = document.getElementById("btnSignin");
+
+btnSignin.addEventListener("click", checkCredentials);
+
+function checkCredentials() {
+  if (mailInput.value == "test@mail.com" && passwordInput.value == "123") {
+
+    const token = "fdskjlfjskldjksljqklùfmezfklcùc";
+    setToken(token)
+
+
+    window.location.replace("/");
+  } else {
+    mailInput.classList.add("is-invalid");
+    passwordInput.classList.add("is-invalid");
+  }
+}
