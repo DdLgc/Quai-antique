ffc92a4a5455fd11e62a0f71ab3ffc0049f460bd
David Le Gouellec <legouellecd@gmail.com>
2026-08-23 18:54:17 +0200

    feat(account): delete authenticated user account

--- js/auth/account.js
+++ js/auth/account.js
@@ -3,6 +3,9 @@ const prenomInput = document.getElementById("PrenomInput");
 const allergieInput = document.getElementById("AllergieInput");
 const nbConvivesInput = document.getElementById("NbConvivesInput");
 const accountForm = document.getElementById("accountForm");
+const btnDeleteAccount = document.getElementById("btnDeleteAccount");
+
+btnDeleteAccount.addEventListener("click", deleteAccount);
 
 getInfoUser().then(user => {
     if (!user) {
@@ -54,6 +57,41 @@ function updateAccount(event) {
             console.error(
                 "Erreur lors de la modification des informations utilisateur",
                 error
+            );
+        });
+}
+
+function deleteAccount() {
+    const confirmation = confirm("Etes-cous sûr de vouloir supprimer définitivement votre compte ?");
+    if (!confirmation) {
+        return;
+    }
+
+    const myHeaders = new Headers();
+    myHeaders.append("X-AUTH-TOKEN", getToken());
+
+    const requestOptions = {
+        method: "DELETE",
+        headers: myHeaders,
+        redirect: "follow"
+    };
+
+    fetch(apiUrl + "account/delete", requestOptions)
+        .then(response => {
+            if (!response.ok) {
+                throw new Error("Impossible de supprimer le compte")
+            }
+
+            if (response.status === 204) {
+                eraseCookie(tokenCookieName);
+                eraseCookie(RoleCookieName);
+
+                window.location.replace("/");
+            }
+        })
+
+        .catch(error => {
+            console.error("Erreur lors de la suppression du compte", error
             );
         });
 }
